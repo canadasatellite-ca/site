bdb28938546efe8856856cd936a5f7f0df84fdaa
Dmitry Fedyuk <dfediuk@gmail.com>
2021-03-21 15:14:23 +0600

    https://github.com/canadasatellite-ca/site/issues/26

--- app/code/Wyomind/SimpleGoogleShopping/Model/Observer.php
+++ app/code/Wyomind/SimpleGoogleShopping/Model/Observer.php
@@ -43,23 +43,20 @@ class Observer
 
     /**
      * Check if google shopping data feeds need to be generated
+	 * 2021-03-21 Dmitry Fedyuk https://www.upwork.com/fl/mage2pro
+	 * "Prevent the Wyomind_SimpleGoogleShopping module from logging successful events to SimpleGoogleShopping-cron.log":
+	 * https://github.com/canadasatellite-ca/site/issues/26
      * @param \Magento\Cron\Model\Schedule $schedule
      */
     public function checkToGenerate(\Magento\Cron\Model\Schedule $schedule)
     {
-
         try {
+        	$hasErrors = false;
             $log = [];
-
-            $this->_logger->notice("-------------------- CRON PROCESS --------------------");
             $log[] = "-------------------- CRON PROCESS --------------------";
-
             $coll = $this->_collectionFactory->create();
-
             $cnt = 0;
-
             $first = true;
-
             foreach ($coll as $feed) {
                 $done = false;
                 try {
@@ -68,34 +65,21 @@ class Observer
                         $feed->loadCustomFunctions();
                         $first = false;
                     }
-
-                    $this->_logger->notice("--> Running profile : " . $feed->getSimplegoogleshoppingFilename() . " [#" . $feed->getSimplegoogleshoppingId() . "] <--");
                     $log[] = "--> Running profile : " . $feed->getSimplegoogleshoppingFilename() . " [#" . $feed->getSimplegoogleshoppingId() . "] <--";
-
                     $cron = [];
-
                     $cron['curent']['localDate'] = $this->_coreDate->date('l Y-m-d H:i:s');
                     $cron['curent']['gmtDate'] = $this->_coreDate->gmtDate('l Y-m-d H:i:s');
                     $cron['curent']['localTime'] = $this->_coreDate->timestamp();
                     $cron['curent']['gmtTime'] = $this->_coreDate->gmtTimestamp();
-
                     $cron['file']['localDate'] = $this->_coreDate->date('l Y-m-d H:i:s', $feed->getSimplegoogleshoppingTime());
                     $cron['file']['gmtDate'] = $feed->getSimplegoogleshoppingTime();
                     $cron['file']['localTime'] = $this->_coreDate->timestamp($feed->getSimplegoogleshoppingTime());
                     $cron['file']['gmtTime'] = strtotime($feed->getSimplegoogleshoppingTime());
-
-
                     $cron['offset'] = $this->_coreDate->getGmtOffset("hours");
-
                     $log[] = '   * Last update : ' . $cron['file']['gmtDate'] . " GMT / " . $cron['file']['localDate'] . ' GMT' . $cron['offset'] . "";
                     $log[] = '   * Current date : ' . $cron['curent']['gmtDate'] . " GMT / " . $cron['curent']['localDate'] . ' GMT' . $cron['offset'] . "";
-                    $this->_logger->notice('   * Last update : ' . $cron['file']['gmtDate'] . " GMT / " . $cron['file']['localDate'] . ' GMT' . $cron['offset']);
-                    $this->_logger->notice('   * Current date : ' . $cron['curent']['gmtDate'] . " GMT / " . $cron['curent']['localDate'] . ' GMT' . $cron['offset']);
-
                     $cronExpr = json_decode($feed->getCronExpr());
-
                     $i = 0;
-
                     if ($cronExpr != null && isset($cronExpr->days)) {
                         foreach ($cronExpr->days as $d) {
                             foreach ($cronExpr->hours as $h) {
@@ -109,86 +93,73 @@ class Observer
                                 }
 
                                 if ($cron['tasks'][$i]['localTime'] >= $cron['file']['localTime'] && $cron['tasks'][$i]['localTime'] <= $cron['curent']['localTime'] && $done != true) {
-                                    $this->_logger->notice('   * Scheduled : ' . ($cron['tasks'][$i]['localDate'] . " GMT" . $cron['offset']));
                                     $log[] = '   * Scheduled : ' . ($cron['tasks'][$i]['localDate'] . " GMT" . $cron['offset']) . "";
-                                    $this->_logger->notice("   * Starting generation");
                                     $result = $feed->generateXml();
                                     if ($result === $feed) {
                                         $done = true;
-                                        $this->_logger->notice("   * EXECUTED!");
                                         $log[] = "   * EXECUTED!";
                                     } else {
-                                        $this->_logger->notice("   * ERROR! " . $result);
                                         $log[] = "   * ERROR! " . $result . "";
+                                        $hasErrors = true;
                                     }
                                     $cnt++;
                                     break 2;
                                 }
-
                                 $i++;
                             }
                         }
                     }
                 } catch (\Exception $e) {
                     $cnt++;
-                    $this->_logger->notice("   * ERROR! " . ($e->getMessage()));
                     $log[] = "   * ERROR! " . ($e->getMessage()) . "";
+                    $hasErrors = true;
                 }
                 if (!$done) {
-                    $this->_logger->notice("   * SKIPPED!");
                     $log[] = "   * SKIPPED!";
                 }
             }
-
-
             if ($this->_coreHelper->getStoreConfig("simplegoogleshopping/settings/enable_reporting")) {
                 $emails = explode(',', $this->_coreHelper->getStoreConfig("simplegoogleshopping/settings/emails"));
                 if (count($emails) > 0) {
                     try {
                         if ($cnt) {
                             $template = "wyomind_simplegoogleshopping_cron_report";
-
                             $transport = $this->_transportBuilder
                                     ->setTemplateIdentifier($template)
-                                    ->setTemplateOptions(
-                                        [
-                                                'area' => \Magento\Backend\App\Area\FrontNameResolver::AREA_CODE,
-                                                'store' => \Magento\Store\Model\Store::DEFAULT_STORE_ID
-                                            ]
-                                    )
-                                    ->setTemplateVars(
-                                        [
-                                                'report' => implode("<br/>", $log),
-                                                'subject' => $this->_coreHelper->getStoreConfig('simplegoogleshopping/settings/report_title')
-                                            ]
-                                    )
-                                    ->setFrom(
-                                        [
-                                                'email' => $this->_coreHelper->getStoreConfig('simplegoogleshopping/settings/sender_email'),
-                                                'name' => $this->_coreHelper->getStoreConfig('simplegoogleshopping/settings/sender_name')
-                                            ]
-                                    )
+                                    ->setTemplateOptions([
+										'area' => \Magento\Backend\App\Area\FrontNameResolver::AREA_CODE,
+										'store' => \Magento\Store\Model\Store::DEFAULT_STORE_ID
+									])
+                                    ->setTemplateVars([
+										'report' => implode("<br/>", $log),
+										'subject' => $this->_coreHelper->getStoreConfig('simplegoogleshopping/settings/report_title')
+									])
+                                    ->setFrom([
+										'email' => $this->_coreHelper->getStoreConfig('simplegoogleshopping/settings/sender_email'),
+										'name' => $this->_coreHelper->getStoreConfig('simplegoogleshopping/settings/sender_name')
+									])
                                     ->addTo($emails[0]);
-
                             $count = count($emails);
                             for ($i = 1; $i < $count; $i++) {
                                 $transport->addCc($emails[$i]);
                             }
-
                             $transport->getTransport()->sendMessage();
                         }
                     } catch (\Exception $e) {
                         $this->_logger->notice('   * EMAIL ERROR! ' . $e->getMessage());
                         $log[] = '   * EMAIL ERROR! ' . ($e->getMessage());
+                        $hasErrors = true;
                     }
                 }
             }
+            if ($hasErrors) {
+            	df_log_l($this, df_cc_n($log));
+			}
         } catch (\Exception $e) {
             $schedule->setStatus('failed');
             $schedule->setMessage($e->getMessage());
             $schedule->save();
-            $this->_logger->notice("MASSIVE ERROR ! ");
-            $this->_logger->notice($e->getMessage());
+            df_log_l($this, $e);
         }
     }
 }
