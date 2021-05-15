246116f59b28833f5f5a64ffdc638be5f04fcdf8
Dmitry Fedyuk <dfediuk@gmail.com>
2021-05-16 05:40:46 +0600

    https://github.com/canadasatellite-ca/site/issues/116

--- app/code/Interactivated/Quotecheckout/Controller/Index/Save.php
+++ app/code/Interactivated/Quotecheckout/Controller/Index/Save.php
@@ -19,13 +19,11 @@ class Save extends \Interactivated\Quotecheckout\Controller\Checkout\Onepage
 	 *
 	 * @return $this
 	 */
-	function execute()
-	{
+	function execute() {
 		$isAjax = (int) $this->getRequest()->getParam('isAjax');
 		if (!$isAjax) {
 			return $this->resultRedirectFactory->create()->setPath('noRoute');
 		}
-
 		$post = $this->getRequest()->getParams();
 		if ($post) {
 			$quote = $this->getOnepage()->getQuote();
@@ -35,43 +33,36 @@ class Save extends \Interactivated\Quotecheckout\Controller\Checkout\Onepage
 			$update->load('onestepcheckout_index_save');
 			$layout->generateXml();
 			$layout->generateElements();
-
 			$updates = json_decode($post['updates']);
 			$this->defineProperties();
-
-			// Gift wrap
 			if (isset($updates->updategiftwrap)) {
 				$iswrap = $updates->updategiftwrap;
-
-				if ($iswrap){
+				if ($iswrap) {
 					$this->_sessionManager->setIsWrap(true);
-				} else {
+				}
+				else {
 					$this->_sessionManager->setIsWrap(false);
 				}
 				$quote->collectTotals()->save();
 			}
-
 			if (isset($updates->updaterewardpoints)) {
 				$rewardpointHelper = $this->_objectManager->get('MW\Rewardpoints\Helper\Data');
-
-				if(!$rewardpointHelper->moduleEnabled()) {
+				if (!$rewardpointHelper->moduleEnabled()) {
 					$html['rewardpoints'] = '';
-				} else {
+				}
+				else {
 					$rewardpoints = $post['rewardpoints'];
 					if($rewardpoints < 0) {
 						$rewardpoints = -$rewardpoints;
 					}
-
 					$step = $rewardpointHelper->getPointStepConfig();
 					$rewardpoints = round(($rewardpoints/$step),0) * $step;
-
 					if($rewardpoints >= 0) {
 						$rewardpointHelper->setPointToCheckOut($rewardpoints);
 						$this->_checkoutSession->getQuote()->collectTotals()->save();
 					}
 				}
 			}
-
 			if (isset($updates->removeproduct)) {
 				$html['removed'] = $this->_removeproduct();
 				if ($html['removed']['error'] == 1 && $html['removed']['msg'] == __('EMPTY')) {
@@ -79,34 +70,21 @@ class Save extends \Interactivated\Quotecheckout\Controller\Checkout\Onepage
 					echo json_encode($html);
 					exit;
 				}
-				/* $html['removed']['html_giftbox'], $html['removed']['html_link']  */
 			}
-
 			if (isset($updates->updatecart)) {
 				$html['cart'] = $this->_updateqty();
-				/** get message of cart */
-				/* $html['cart']['msg'], $html['cart']['error']  */
 			}
-
 			if (isset($updates->updatecouponcode)) {
 				$html['coupon'] = $this->_updatecoupon();
-				/** get html message of coupon */
-				/* $html['coupon']['html'] */
 			}
-
 			if (isset($updates->updatebillingaddress)) {
 				$html['billing'] = $this->_updatebillingform(isset($updates->changebycountry) ? false : true);
-				/** get html billing address */
 			}
-
 			if (isset($updates->updateshippingaddress)) {
 				$html['shipping'] = $this->_updateshippingform();
-				/** get html shipping address */
 			}
-
 			if (isset($updates->updateshippingtype)) {
 				$this->_updateshippingtype();
-				/** get html shipping medthod */
 				$shippingMethodBlock = $layout->createBlock(
 					'Interactivated\Quotecheckout\Block\Checkout\Onepage\Shipping\Method\Available'
 				)->setTemplate(
@@ -114,41 +92,33 @@ class Save extends \Interactivated\Quotecheckout\Controller\Checkout\Onepage
 				);
 				$html['shipping_method'] = $shippingMethodBlock->toHtml();
 			}
-
 			if (isset($updates->updateshippingmethod)) {
 				if (isset($post['shipping_method']) && $post['shipping_method'] != '') {
 					$data   = $post['shipping_method'];
 					$result = $this->getOnepage()->saveShippingMethod($data);
 					if (!$result) {
 						$eventManager = $this->_objectManager->get('Magento\Framework\Event\ManagerInterface');
-						$eventManager->dispatch(
-							'checkout_controller_onepage_save_shipping_method',
-							[
-								'request' => $this->getRequest(),
-								'quote'   => $quote
-							]
-						);
+						$eventManager->dispatch('checkout_controller_onepage_save_shipping_method', [
+							'quote'   => $quote, 'request' => $this->getRequest()
+						]);
 						$quote->collectTotals();
 					}
-
 					$quote->collectTotals()->save();
 				}
 			}
-
-			// Save cart to calculate tax
 			if (!$this->_customerSession->isLoggedIn()) {
 				$cart = $this->_objectManager->get('Cart2Quote\Quotation\Model\QuotationCart');
 				$cart->save();
 				$this->_checkoutSession->setCartWasUpdated(true);
-			} else {
-				// If customer logged in, check using new address or not
+			}
+			else {
 				if (isset($post['billing']) && isset($post['shipping'])) {
 					if (isset($post['ship_to_same_address']) && $post['ship_to_same_address'] == '1') {
 						$hasAddress = isset($post['billing_address_id']) ? $post['billing_address_id'] : '';
-					} else {
+					}
+					else {
 						$hasAddress = isset($post['shipping_address_id']) ? $post['shipping_address_id'] : '';
 					}
-
 					if (!$hasAddress) {
 						$cart = $this->_objectManager->get('Cart2Quote\Quotation\Model\QuotationCart');
 						$cart->save();
@@ -156,36 +126,31 @@ class Save extends \Interactivated\Quotecheckout\Controller\Checkout\Onepage
 					}
 				}
 			}
-
 			if (isset($updates->updatepaymenttype)) {
-				/** get html all payment type */
 				$html['payment_method'] = $this->renderPaymentForm();
 			}
-
 			if (isset($updates->updatepaymentmethod)) {
 				if (isset($post['payment']['method']) && $post['payment']['method'] != '') {
 					$data = $post['payment'];
 					$this->getOnepage()->savePayment($data);
 				}
 			}
-
 			if (isset($updates->checkvat)) {
 				$html['VAT'] = $this->getVat();
 			}
-
 			$totalsBlock = $layout->createBlock('Magento\Checkout\Block\Cart\Totals');
 			$totalsBlock->setTemplate('Interactivated_Quotecheckout::dashboard/onepage/review/totals.phtml');
 			$this->getColspanTotal();
-
 			$html = [
 				'vat'                 => (isset($html['VAT'])) ? $html['VAT'] : "",
 				'items'               => $this->_objectManager->get('Magento\Checkout\Model\Cart')->getItemsQty(),
 				'review_info'         => $this->renderReview(),
 				'totals'              => $this->_htmlUpdatecart . $totalsBlock->renderTotals(null, $this->_colspan),
 				'totals_footer'       => $totalsBlock->renderTotals('footer', $this->_colspan),
-				'earn_points'         => ($this->isRWPRunning()) ? $this->_objectManager->get(
-						'Magento\Config\Helper\Mwrewardpoints'
-					)->earnPointsOnepageReviewRewardPoints() : '',
+				'earn_points'         => ($this->isRWPRunning())
+					? $this->_objectManager->get('Magento\Config\Helper\Mwrewardpoints')
+						->earnPointsOnepageReviewRewardPoints()
+					: '',
 				'billing'             => (isset($html['billing'])) ? $html['billing'] : "",
 				'shipping'            => (isset($html['shipping'])) ? $html['shipping'] : "",
 				'coupon'              => (isset($html['coupon'])) ? $html['coupon'] : "",
@@ -194,7 +159,6 @@ class Save extends \Interactivated\Quotecheckout\Controller\Checkout\Onepage
 				'item_payment_method' => (isset($html['item_payment_method'])) ? $html['item_payment_method'] : "",
 				'cart'                => (isset($html['cart'])) ? $html['cart'] : ""
 			];
-
 			echo json_encode($html);
 			exit;
 		}
