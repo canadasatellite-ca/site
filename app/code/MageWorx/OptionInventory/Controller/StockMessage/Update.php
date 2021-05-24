e88dfc61d482c582e295de510361b586d2ffde6f
Dmitry Fedyuk <dfediuk@gmail.com>
2021-05-24 07:12:13 +0600

    https://github.com/canadasatellite-ca/site/issues/126

--- app/code/MageWorx/OptionInventory/Controller/StockMessage/Update.php
+++ app/code/MageWorx/OptionInventory/Controller/StockMessage/Update.php
@@ -1,5 +1,6 @@
 <?php
 namespace MageWorx\OptionInventory\Controller\StockMessage;
+use Df\Framework\W\Result\Json;
 use Magento\Framework\App\Action\Action;
 class Update extends Action {
     /**
@@ -25,10 +26,9 @@ class Update extends Action {
 	 * 2021-05-24 Dmitry Fedyuk https://www.upwork.com/fl/mage2pro
 	 * "Refactor the `MageWorx_OptionInventory` module": https://github.com/canadasatellite-ca/site/issues/126
 	 * @used-by \Magento\Framework\App\Action\Action::dispatch()
+	 * @return Json
      */
-    function execute() {
-        df_response()->setBody(df_json_encode([
-        	'result' => $this->stockProvider->updateOptionsStockMessage(df_json_decode(df_request('opConfig')))
-		]));
-    }
+    function execute() {return Json::i([
+    	'result' => $this->stockProvider->updateOptionsStockMessage(df_json_decode(df_request('opConfig')))
+	]);}
 }
