4e60b4cc57ad52b14a568f3f0757403870a44864
Dmitry Fedyuk <dfediuk@gmail.com>
2021-06-01 09:09:28 +0600

    https://github.com/canadasatellite-ca/site/issues/132

--- app/code/CanadaSatellite/Theme/Model/ResourceModel/Card/Collection.php
+++ app/code/CanadaSatellite/Theme/Model/ResourceModel/Card/Collection.php
@@ -7,94 +7,94 @@ namespace CanadaSatellite\Theme\Model\ResourceModel\Card;
  */
 class Collection extends \Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection
 {
-    /**
-     * REST API for MS Dynamics
-     *
-     * @var \CanadaSatellite\DynamicsIntegration\Rest\RestApi
-     */
-    protected $_restApi;
+	/**
+	 * REST API for MS Dynamics
+	 *
+	 * @var \CanadaSatellite\DynamicsIntegration\Rest\RestApi
+	 */
+	protected $_restApi;
 
-    protected $_magentoCustomerId;
+	protected $_magentoCustomerId;
 
-    /**
-     * @param \Magento\Framework\Data\Collection\EntityFactoryInterface $entityFactory
-     * @param \Psr\Log\LoggerInterface $logger
-     * @param \Magento\Framework\Data\Collection\Db\FetchStrategyInterface $fetchStrategy
-     * @param \Magento\Framework\Event\ManagerInterface $eventManager
-     * @param \Magento\Framework\DB\Adapter\AdapterInterface $connection
-     * @param \Magento\Framework\Model\ResourceModel\Db\AbstractDb $resource
-     */
-    public function __construct(
-        \Magento\Framework\Data\Collection\EntityFactoryInterface $entityFactory,
-        \Psr\Log\LoggerInterface $logger,
-        \Magento\Framework\Data\Collection\Db\FetchStrategyInterface $fetchStrategy,
-        \Magento\Framework\Event\ManagerInterface $eventManager,
-        \CanadaSatellite\DynamicsIntegration\Rest\RestApi $restApi,
-        \Magento\Framework\DB\Adapter\AdapterInterface $connection = null,
-        \Magento\Framework\Model\ResourceModel\Db\AbstractDb $resource = null
-    ) {
-        $this->_restApi = $restApi;
-        parent::__construct($entityFactory, $logger, $fetchStrategy, $eventManager, $connection, $resource);
-    }
+	/**
+	 * @param \Magento\Framework\Data\Collection\EntityFactoryInterface $entityFactory
+	 * @param \Psr\Log\LoggerInterface $logger
+	 * @param \Magento\Framework\Data\Collection\Db\FetchStrategyInterface $fetchStrategy
+	 * @param \Magento\Framework\Event\ManagerInterface $eventManager
+	 * @param \Magento\Framework\DB\Adapter\AdapterInterface $connection
+	 * @param \Magento\Framework\Model\ResourceModel\Db\AbstractDb $resource
+	 */
+	public function __construct(
+		\Magento\Framework\Data\Collection\EntityFactoryInterface $entityFactory,
+		\Psr\Log\LoggerInterface $logger,
+		\Magento\Framework\Data\Collection\Db\FetchStrategyInterface $fetchStrategy,
+		\Magento\Framework\Event\ManagerInterface $eventManager,
+		\CanadaSatellite\DynamicsIntegration\Rest\RestApi $restApi,
+		\Magento\Framework\DB\Adapter\AdapterInterface $connection = null,
+		\Magento\Framework\Model\ResourceModel\Db\AbstractDb $resource = null
+	) {
+		$this->_restApi = $restApi;
+		parent::__construct($entityFactory, $logger, $fetchStrategy, $eventManager, $connection, $resource);
+	}
 
-    /**
-     * Collection model initialization
-     *
-     * @return void
-     */
-    protected function _construct()
-    {
-        $this->_init('CanadaSatellite\Theme\Model\Card', 'CanadaSatellite\Theme\Model\ResourceModel\Card');
-    }
+	/**
+	 * Collection model initialization
+	 *
+	 * @return void
+	 */
+	protected function _construct()
+	{
+		$this->_init('CanadaSatellite\Theme\Model\Card', 'CanadaSatellite\Theme\Model\ResourceModel\Card');
+	}
 
-    /**
-     * Add customer filter
-     *
-     * @param int $magentoCustomerId
-     * @return \CanadaSatellite\Theme\Model\ResourceModel\Sim\Collection
-     */
-    public function addCustomerFilter($magentoCustomerId)
-    {
-        $this->_magentoCustomerId = $magentoCustomerId;
-        return $this;
-    }
+	/**
+	 * Add customer filter
+	 *
+	 * @param int $magentoCustomerId
+	 * @return \CanadaSatellite\Theme\Model\ResourceModel\Sim\Collection
+	 */
+	public function addCustomerFilter($magentoCustomerId)
+	{
+		$this->_magentoCustomerId = $magentoCustomerId;
+		return $this;
+	}
 
-    /**
-     * Load data with filter in place
-     * it is not designed to load without customerId filter!
-     *
-     * @param   bool $printQuery
-     * @param   bool $logQuery
-     * @return  $this
-     */
-    public function loadWithFilter($printQuery = false, $logQuery = false)
-    {
-        $this->_beforeLoad();
-        //$this->printLogQuery($printQuery, $logQuery);
-        // FIXME retrieve only one page? but _totalRecords
-        $data = $this->_restApi->getCardsByCustomerId($this->_magentoCustomerId);
-        $this->resetData();
-        $this->_totalRecords = count($data);
-        if (is_array($data)) {
-            if ($this->_pageSize) {
-                $start = $this->_pageSize*($this->getCurPage() - 1);
-                $end = min($start + $this->_pageSize, $this->_totalRecords);
-            } else {
-                $start = 0;
-                $end = $this->_totalRecords;
-            }
-            for($i = $start; $i < $end; $i++) {
-                $item = $this->getNewEmptyItem();
-                if ($this->getIdFieldName()) {
-                    $item->setIdFieldName($this->getIdFieldName());
-                }
-                $item->addData((array)$data[$i]);
-                $this->beforeAddLoadedItem($item);
-                $this->addItem($item);
-            }
-        }
-        $this->_setIsLoaded();
-        $this->_afterLoad();
-        return $this;
-    }
+	/**
+	 * Load data with filter in place
+	 * it is not designed to load without customerId filter!
+	 *
+	 * @param   bool $printQuery
+	 * @param   bool $logQuery
+	 * @return  $this
+	 */
+	public function loadWithFilter($printQuery = false, $logQuery = false)
+	{
+		$this->_beforeLoad();
+		//$this->printLogQuery($printQuery, $logQuery);
+		// FIXME retrieve only one page? but _totalRecords
+		$data = $this->_restApi->getCardsByCustomerId($this->_magentoCustomerId);
+		$this->resetData();
+		$this->_totalRecords = count($data);
+		if (is_array($data)) {
+			if ($this->_pageSize) {
+				$start = $this->_pageSize*($this->getCurPage() - 1);
+				$end = min($start + $this->_pageSize, $this->_totalRecords);
+			} else {
+				$start = 0;
+				$end = $this->_totalRecords;
+			}
+			for($i = $start; $i < $end; $i++) {
+				$item = $this->getNewEmptyItem();
+				if ($this->getIdFieldName()) {
+					$item->setIdFieldName($this->getIdFieldName());
+				}
+				$item->addData((array)$data[$i]);
+				$this->beforeAddLoadedItem($item);
+				$this->addItem($item);
+			}
+		}
+		$this->_setIsLoaded();
+		$this->_afterLoad();
+		return $this;
+	}
 }
