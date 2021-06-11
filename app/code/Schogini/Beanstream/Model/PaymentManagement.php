ebf59cb1c5854f9671a7430552546b1d7693de02
Dmitry Fedyuk <dfediuk@gmail.com>
2021-06-11 12:25:41 +0600

    https://github.com/canadasatellite-ca/site/issues/176

--- app/code/Schogini/Beanstream/Model/PaymentManagement.php
+++ app/code/Schogini/Beanstream/Model/PaymentManagement.php
@@ -1,21 +1,15 @@
 <?php
 namespace Schogini\Beanstream\Model;
-use Magento\Framework\Exception\CouldNotSaveException;
+use Magento\Framework\Exception\CouldNotSaveException as CNSE;
+use Magento\Quote\Api\Data\AddressInterface as IA;
+use Magento\Quote\Api\Data\PaymentInterface as IP;
 # 2021-06-11 Dmitry Fedyuk https://www.upwork.com/fl/mage2pro
 # "Refactor the `Schogini_Beanstream` module": https://github.com/canadasatellite-ca/site/issues/176
 class PaymentManagement extends \Magento\Checkout\Model\PaymentInformationManagement{
-	function savePaymentInformationAndPlaceOrder(
-		$spb2524e
-		,\Magento\Quote\Api\Data\PaymentInterface $spf269e4
-		,\Magento\Quote\Api\Data\AddressInterface $spc02997 = null
-	)
-	{
-		$this->savePaymentInformation($spb2524e, $spf269e4, $spc02997);
-		try {
-			$spf504cb = $this->cartManagement->placeOrder($spb2524e);
-		} catch (\Exception $sp8303e0) {
-			throw new CouldNotSaveException(__('Cannot place order: ' . $sp8303e0->getMessage()), $sp8303e0);
-		}
-		return $spf504cb;
+	function savePaymentInformationAndPlaceOrder($v, IP $p, IA $a = null) {
+		$this->savePaymentInformation($v, $p, $a);
+		try {$r = $this->cartManagement->placeOrder($v);}
+		catch (\Exception $e) {throw new CNSE(__('Cannot place order: ' . $e->getMessage()), $e);}
+		return $r;
 	}
 }
