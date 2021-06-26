c6355ffd2fd9a4b545fcac18a927fdd1662b5815
Dmitry Fedyuk <dfediuk@gmail.com>
2021-06-26 10:20:41 +0600

    https://github.com/canadasatellite-ca/site/issues/190

--- app/code/Mageplaza/Blog/Block/Tag/Listpost.php
+++ app/code/Mageplaza/Blog/Block/Tag/Listpost.php
@@ -1,37 +1,14 @@
 <?php
-/**
- * Mageplaza
- *
- * NOTICE OF LICENSE
- *
- * This source file is subject to the Mageplaza.com license that is
- * available through the world-wide-web at this URL:
- * https://www.mageplaza.com/LICENSE.txt
- *
- * DISCLAIMER
- *
- * Do not edit or add to this file if you wish to upgrade this extension to newer
- * version in the future.
- *
- * @category    Mageplaza
- * @package     Mageplaza_Blog
- * @copyright   Copyright (c) 2016 Mageplaza (http://www.mageplaza.com/)
- * @license     https://www.mageplaza.com/LICENSE.txt
- */
 namespace Mageplaza\Blog\Block\Tag;
-
 use Mageplaza\Blog\Block\Frontend;
+class Listpost extends Frontend {
+	/**
+	 * @used-by app/code/Mageplaza/Blog/view/frontend/templates/category/post/list.phtml
+	 * @return mixed
+	 */
+	function getPostList() {return
+		$this->getBlogPagination(\Mageplaza\Blog\Helper\Data::TAG, $this->getRequest()->getParam('id'))
+	;}
 
-class Listpost extends Frontend
-{
-
-    function getPostList()
-    {
-        return $this->getBlogPagination(\Mageplaza\Blog\Helper\Data::TAG, $this->getRequest()->getParam('id'));
-    }
-
-    function checkRss()
-    {
-        return $this->helperData->getBlogUrl('post/rss');
-    }
+	function checkRss() {return $this->helperData->getBlogUrl('post/rss');}
 }
