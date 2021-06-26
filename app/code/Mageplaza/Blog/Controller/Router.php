474d0651fb92d076c1002e36b9b9293e75dc805e
Dmitry Fedyuk <dfediuk@gmail.com>
2021-06-27 04:50:35 +0600

    https://github.com/canadasatellite-ca/site/issues/191

--- app/code/Mageplaza/Blog/Controller/Router.php
+++ app/code/Mageplaza/Blog/Controller/Router.php
@@ -32,76 +32,83 @@ class Router implements IRouter {
 	function match(IRequest $req) {
 		$r = null; /** @var Forward|null $r */
 		$h = df_o(H::class); /** @var H $h */
-		# 2021-06-26 E.g.: «blog/tag/space-force».
-		$path = trim($req->getPathInfo(), '/'); /** @var string $path */
-		$pathA  = explode('/', $path); /** @var string[] $pathA */
 		# 2021-06-26 E.g.: «blog».
 		$prefix = $h->getBlogConfig('general/url_prefix') ?: H::DEFAULT_URL_PREFIX; /** @var string $prefix */
-		$routeSize  = sizeof($pathA);
-		if ($h->isEnabled() && df_between($routeSize, 1, 3) && $prefix === array_shift($pathA)) {
-			$req->setModuleName('mpblog')->setAlias(Url::REWRITE_REQUEST_PATH_ALIAS, $path);
-			$params = [];
-			if (!($controller = array_shift($pathA))) { /** @var string $controller */
-				$r = $this->forward($req, 'post', 'index');
-			}
-			else {
-				/**
-				 * 2021-06-26 Dmitry Fedyuk https://www.upwork.com/fl/mage2pro
-				 * "`Mageplaza_Blog` does not properly handle `/blog/tag/<tag>/` requests for an existing `<tag>`
-				 * if it contains a space (e.g.: `/blog/tag/satellite%20tv/`)":
-				 * https://github.com/canadasatellite-ca/site/issues/189
-				 * @return string
-				 */
-				$pathF = function() use(&$pathA) {return urldecode(array_shift($pathA));};
-				if ('tag' === $controller) {
-					$tag = $h->getTagByParam('url_key', $pathF()); /** @var Tag $tag */
-					$r = ($id = $tag->getId())
-						? $this->forward($req, $controller, 'view', ['id' => $id])
-						# 2021-06-27 Dmitry Fedyuk https://www.upwork.com/fl/mage2pro
-						# "`Mageplaza_Blog`: `/blog/tag/<tag>/` should be redirected to `/blog` for unexisting tags":
-						# https://github.com/canadasatellite-ca/site/issues/188
-						: df_router_redirect($req, $prefix)
-					;
+		# 2021-06-27 Dmitry Fedyuk https://www.upwork.com/fl/mage2pro
+		# "`Mageplaza_Blog`: redirect `/blog/` to `/blog`": https://github.com/canadasatellite-ca/site/issues/191
+		if ("/$prefix/" === $req->getPathInfo()) {
+			$r = df_router_redirect($req, $prefix);
+		}
+		else {
+			# 2021-06-26 E.g.: «blog/tag/space-force».
+			$path = trim($req->getPathInfo(), '/'); /** @var string $path */
+			$pathA  = explode('/', $path); /** @var string[] $pathA */
+			$routeSize  = sizeof($pathA);
+			if ($h->isEnabled() && df_between($routeSize, 1, 3) && $prefix === array_shift($pathA)) {
+				$req->setModuleName('mpblog')->setAlias(Url::REWRITE_REQUEST_PATH_ALIAS, $path);
+				$params = [];
+				if (!($controller = array_shift($pathA))) { /** @var string $controller */
+					$r = $this->forward($req, 'post', 'index');
 				}
 				else {
-					switch ($controller) {
-						case 'post':
-							$action = $pathF() ?: 'index';
-							if (!in_array($action, ['index', 'rss'])) {
-								$post = $h->getPostByUrl($action);
+					/**
+					 * 2021-06-26 Dmitry Fedyuk https://www.upwork.com/fl/mage2pro
+					 * "`Mageplaza_Blog` does not properly handle `/blog/tag/<tag>/` requests for an existing `<tag>`
+					 * if it contains a space (e.g.: `/blog/tag/satellite%20tv/`)":
+					 * https://github.com/canadasatellite-ca/site/issues/189
+					 * @return string
+					 */
+					$pathF = function() use(&$pathA) {return urldecode(array_shift($pathA));};
+					if ('tag' === $controller) {
+						$tag = $h->getTagByParam('url_key', $pathF()); /** @var Tag $tag */
+						$r = ($id = $tag->getId())
+							? $this->forward($req, $controller, 'view', ['id' => $id])
+							# 2021-06-27 Dmitry Fedyuk https://www.upwork.com/fl/mage2pro
+							# "`Mageplaza_Blog`: `/blog/tag/<tag>/` should be redirected to `/blog` for unexisting tags":
+							# https://github.com/canadasatellite-ca/site/issues/188
+							: df_router_redirect($req, $prefix)
+						;
+					}
+					else {
+						switch ($controller) {
+							case 'post':
+								$action = $pathF() ?: 'index';
+								if (!in_array($action, ['index', 'rss'])) {
+									$post = $h->getPostByUrl($action);
+									$action = 'view';
+									$params = ['id' => $post->getId()];
+								}
+								break;
+							case 'category':
+								$action = $pathF() ?: 'index';
+								if (!in_array($action, ['index', 'rss'])) {
+									$category = $h->getCategoryByParam('url_key', $action);
+									$action = 'view';
+									$params = ['id' => $category->getId()];
+								}
+								break;
+							case 'topic':
+								$topic = $h->getTopicByParam('url_key', $pathF());
+								$action = 'view';
+								$params = ['id' => $topic->getId()];
+								break;
+							case 'sitemap':
+								$action = 'index';
+								break;
+							case 'author':
+							case 'month':
+								$author  = $h->getAuthorByParam('url_key', $pathF());
+								$action = 'view';
+								$params = ['id' => $author->getId()];
+								break;
+							default:
+								$post = $h->getPostByUrl($controller);
+								$controller = 'post';
 								$action = 'view';
 								$params = ['id' => $post->getId()];
-							}
-							break;
-						case 'category':
-							$action = $pathF() ?: 'index';
-							if (!in_array($action, ['index', 'rss'])) {
-								$category = $h->getCategoryByParam('url_key', $action);
-								$action = 'view';
-								$params = ['id' => $category->getId()];
-							}
-							break;
-						case 'topic':
-							$topic = $h->getTopicByParam('url_key', $pathF());
-							$action = 'view';
-							$params = ['id' => $topic->getId()];
-							break;
-						case 'sitemap':
-							$action = 'index';
-							break;
-						case 'author':
-						case 'month':
-							$author  = $h->getAuthorByParam('url_key', $pathF());
-							$action = 'view';
-							$params = ['id' => $author->getId()];
-							break;
-						default:
-							$post = $h->getPostByUrl($controller);
-							$controller = 'post';
-							$action = 'view';
-							$params = ['id' => $post->getId()];
+						}
+						$r = $this->forward($req, $controller, $action, $params);
 					}
-					$r = $this->forward($req, $controller, $action, $params);
 				}
 			}
 		}
