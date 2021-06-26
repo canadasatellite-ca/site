44392f245c4b41617b88a28c2916b1dd311b1504
Dmitry Fedyuk <dfediuk@gmail.com>
2021-06-26 16:20:45 +0600

    https://github.com/canadasatellite-ca/site/issues/190

--- app/code/Mageplaza/Blog/Block/Tag/Listpost.php
+++ app/code/Mageplaza/Blog/Block/Tag/Listpost.php
@@ -1,14 +1,13 @@
 <?php
 namespace Mageplaza\Blog\Block\Tag;
 use Mageplaza\Blog\Block\Frontend;
+use Mageplaza\Blog\Helper\Data;
 class Listpost extends Frontend {
 	/**
 	 * @used-by app/code/Mageplaza/Blog/view/frontend/templates/category/post/list.phtml
 	 * @return mixed
 	 */
-	function getPostList() {return
-		$this->getBlogPagination(\Mageplaza\Blog\Helper\Data::TAG, $this->getRequest()->getParam('id'))
-	;}
+	function getPostList() {return $this->getBlogPagination(Data::TAG, $this->getRequest()->getParam('id'));}
 
 	function checkRss() {return $this->helperData->getBlogUrl('post/rss');}
 }
