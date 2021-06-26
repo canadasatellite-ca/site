7f9f3dc7d72d643d8756ca4d7fe9f5975472f2dc
Dmitry Fedyuk <dfediuk@gmail.com>
2021-06-26 19:19:05 +0600

    https://github.com/canadasatellite-ca/site/issues/190

--- app/code/Mageplaza/Blog/Controller/Router.php
+++ app/code/Mageplaza/Blog/Controller/Router.php
@@ -23,74 +23,77 @@ class Router implements \Magento\Framework\App\RouterInterface {
 	 * @see \Magento\Framework\App\RouterInterface::match()
 	 * @used-by \Magento\Framework\App\FrontController::dispatch()
 	 * @param IRequest|Http $req
-	 * @return bool
+	 * @return Forward|null
 	 */
 	function match(IRequest $req) {
+		$r = null; /** @var Forward|null $r */
 		$h = df_o(H::class); /** @var H $h */
 		$identifier = trim($req->getPathInfo(), '/');
 		$routePath  = explode('/', $identifier);
 		$urlPrefix = $h->getBlogConfig('general/url_prefix') ?: H::DEFAULT_URL_PREFIX;
 		$routeSize  = sizeof($routePath);
-		if (!$h->isEnabled() || !$routeSize || ($routeSize > 3) || (array_shift($routePath) != $urlPrefix)) {
-			return null;
-		}
-		$req->setModuleName('mpblog')->setAlias(\Magento\Framework\Url::REWRITE_REQUEST_PATH_ALIAS, $identifier);
-		$params = [];
-		$controller = array_shift($routePath);
-		if (!$controller) {
-			return $this->forward($req, 'post', 'index');
-		}
-		/**
-		 * 2021-06-26 Dmitry Fedyuk https://www.upwork.com/fl/mage2pro
-		 * "`Mageplaza_Blog` does not properly handle `/blog/tag/<tag>/` requests for an existing `<tag>`
-		 * if it contains a space (e.g.: `/blog/tag/satellite%20tv/`)":
-		 * https://github.com/canadasatellite-ca/site/issues/189
-		 * @return string
-		 */
-		$pathF = function() use(&$routePath) {return urldecode(array_shift($routePath));};
-		switch ($controller) {
-			case 'post':
-				$action = $pathF() ?: 'index';
-				if (!in_array($action, ['index', 'rss'])) {
-					$post = $h->getPostByUrl($action);
-					$action = 'view';
-					$params = ['id' => $post->getId()];
+		if ($h->isEnabled() && $routeSize && $routeSize < 4 && $urlPrefix === array_shift($routePath)) {
+			$req->setModuleName('mpblog')->setAlias(\Magento\Framework\Url::REWRITE_REQUEST_PATH_ALIAS, $identifier);
+			$params = [];
+			$controller = array_shift($routePath);
+			if (!$controller) {
+				$r = $this->forward($req, 'post', 'index');
+			}
+			else {
+				/**
+				 * 2021-06-26 Dmitry Fedyuk https://www.upwork.com/fl/mage2pro
+				 * "`Mageplaza_Blog` does not properly handle `/blog/tag/<tag>/` requests for an existing `<tag>`
+				 * if it contains a space (e.g.: `/blog/tag/satellite%20tv/`)":
+				 * https://github.com/canadasatellite-ca/site/issues/189
+				 * @return string
+				 */
+				$pathF = function() use(&$routePath) {return urldecode(array_shift($routePath));};
+				switch ($controller) {
+					case 'post':
+						$action = $pathF() ?: 'index';
+						if (!in_array($action, ['index', 'rss'])) {
+							$post = $h->getPostByUrl($action);
+							$action = 'view';
+							$params = ['id' => $post->getId()];
+						}
+						break;
+					case 'category':
+						$action = $pathF() ?: 'index';
+						if (!in_array($action, ['index', 'rss'])) {
+							$category = $h->getCategoryByParam('url_key', $action);
+							$action = 'view';
+							$params = ['id' => $category->getId()];
+						}
+						break;
+					case 'tag':
+						$tag = $h->getTagByParam('url_key', $pathF());
+						$action = 'view';
+						$params = ['id' => $tag->getId()];
+						break;
+					case 'topic':
+						$topic = $h->getTopicByParam('url_key', $pathF());
+						$action = 'view';
+						$params = ['id' => $topic->getId()];
+						break;
+					case 'sitemap':
+						$action = 'index';
+						break;
+					case 'author':
+					case 'month':
+						$author  = $h->getAuthorByParam('url_key', $pathF());
+						$action = 'view';
+						$params = ['id' => $author->getId()];
+						break;
+					default:
+						$post = $h->getPostByUrl($controller);
+						$controller = 'post';
+						$action = 'view';
+						$params = ['id' => $post->getId()];
 				}
-				break;
-			case 'category':
-				$action = $pathF() ?: 'index';
-				if (!in_array($action, ['index', 'rss'])) {
-					$category = $h->getCategoryByParam('url_key', $action);
-					$action = 'view';
-					$params = ['id' => $category->getId()];
-				}
-				break;
-			case 'tag':
-				$tag = $h->getTagByParam('url_key', $pathF());
-				$action = 'view';
-				$params = ['id' => $tag->getId()];
-				break;
-			case 'topic':
-				$topic = $h->getTopicByParam('url_key', $pathF());
-				$action = 'view';
-				$params = ['id' => $topic->getId()];
-				break;
-			case 'sitemap':
-				$action = 'index';
-				break;
-			case 'author':
-			case 'month':
-				$author  = $h->getAuthorByParam('url_key', $pathF());
-				$action = 'view';
-				$params = ['id' => $author->getId()];
-				break;
-			default:
-				$post = $h->getPostByUrl($controller);
-				$controller = 'post';
-				$action = 'view';
-				$params = ['id' => $post->getId()];
+				$r = $this->forward($req, $controller, $action, $params);
+			}
 		}
-		return $this->forward($req, $controller, $action, $params);
+		return $r;
 	}
 
 	/**
