55553708d860b3154146e1450e968a0201439fa4
Dmitry Fedyuk <dfediuk@gmail.com>
2021-07-14 06:08:02 +0600

    https://github.com/canadasatellite-ca/bambora/issues/1

--- app/code/Schogini/Beanstream/Model/BeanstreamException.php
+++
@@ -1,2 +0,0 @@
-<?php
-namespace Schogini\Beanstream\Model; class BeanstreamException extends \Magento\Framework\Exception\LocalizedException { const AUTHENTICATION_ERROR = 'An authentication error occurred.'; }
--- app/code/Schogini/Beanstream/Model/Source/Paymentaction.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Schogini\Beanstream\Model\Source;
-class Paymentaction
-{
-    function toOptionArray()
-    {
-        return array(array('value' => \Magento\Payment\Model\Method\Cc::ACTION_AUTHORIZE, 'label' => 'Authorize Only'), array('value' => \Magento\Payment\Model\Method\Cc::ACTION_AUTHORIZE_CAPTURE, 'label' => 'Sale'));
-    }
-}
